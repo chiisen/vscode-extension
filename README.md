0a7c4bd9fa3688229839b01c3ff48568269d5c12
sam <samliao1@auforce.com>
2024-07-10 15:46:06 +0800

    ✨ feat: 新增 help 說明提示

--- README.md
+++ README.md
@@ -59,3 +59,10 @@ ERROR  The 'create-publisher' command is no longer available. You can create a p
 
 Details 裡面有 publisher id: vscode-extension-hello-world-1221
 
+# VSCode 安裝 VSCode Extension (*.vsix)
+![VSCodeExtensionIbstallVsixFile](./docs/images/install_vsix_file.png)
+1. 點擊左下角的擴展圖示（Extensions）圖示，或使用快捷鍵 Ctrl+Shift+X。
+2. 在擴展側邊欄中，點擊右上角的三個點（More Actions）圖示，然後選擇「Install from VSIX...」。
+3. 瀏覽到您存儲的 .vsix 檔案所在位置，選擇該檔案，然後點擊「安裝」。
+4. 安裝完成後，您的新擴展就會出現在您的擴展清單中。
+
--- src/extension.ts
+++ src/extension.ts
@@ -1,51 +1,58 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // VSCode Extension 的名稱
+  const vscodeExtensionName = "vscode-extension-hello-world";
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('👍恭喜, 你的套件 "Hello World" 現在激活了!');// 寫到【偵錯主控台】
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    `👍恭喜, 你的 VSCode Extension: "${vscodeExtensionName}" 現在激活了❗`
+  ); // 寫到【偵錯主控台】
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable1 = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('✡️Hello World from vscode-extension!');
-		
-		console.log('👍Hello World from vscode-extension!');// 寫到【偵錯主控台】
-	});
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable1 = vscode.commands.registerCommand("extension.help", () => {
+    // The code you place here will be executed every time your command is executed
+    // Display a message box to the user
+    vscode.window.showInformationMessage(
+      `👍 你好，這是 VSCode Extension: "${vscodeExtensionName}" ❗`
+    ); // 顯示在右下角的通知訊息
 
-	context.subscriptions.push(disposable1);
+    console.log(`👍 開啟 VSCode Extension: "${vscodeExtensionName}" ❗`); // 寫到【偵錯主控台】
+  });
 
-	let disposable2 = vscode.commands.registerCommand('extension.createComponent', async () => {
-		const componentName = await vscode.window.showInputBox({
-			prompt: `請輸入套件名稱:`,
-			ignoreFocusOut: true,
-			valueSelection: [-1, -1],
-		});
-		if (!componentName) {
-			return;
-		}
-		const dir =
-			(await vscode.window.showInputBox({
-				value: 'src',
-				prompt: `請輸入套件名稱:`,
-				ignoreFocusOut: true,
-				valueSelection: [-1, -1],
-			})) || ``;
-			vscode.window.showInformationMessage('✡️剛剛輸入的套件名稱: ' + dir);
-	});
+  context.subscriptions.push(disposable1);
 
-	context.subscriptions.push(disposable2);
+  let disposable2 = vscode.commands.registerCommand(
+    "extension.createComponent",
+    async () => {
+      const componentName = await vscode.window.showInputBox({
+        prompt: `請輸入套件名稱:`,
+        ignoreFocusOut: true,
+        valueSelection: [-1, -1],
+      });
+      if (!componentName) {
+        return;
+      }
+      const dir =
+        (await vscode.window.showInputBox({
+          value: "src",
+          prompt: `請輸入套件名稱:`,
+          ignoreFocusOut: true,
+          valueSelection: [-1, -1],
+        })) || ``;
+      vscode.window.showInformationMessage("✡️剛剛輸入的套件名稱: " + dir);
+    }
+  );
+
+  context.subscriptions.push(disposable2);
 }
 
-
-
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
